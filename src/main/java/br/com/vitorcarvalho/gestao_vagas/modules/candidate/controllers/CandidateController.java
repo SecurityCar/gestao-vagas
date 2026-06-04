8319efe6a4094fe8d7c3f9b359d245619b8a9a56
Vitor Carvalho <vitorclocked@gmail.com>
2026-06-04 15:37:05 -0300

    feat: cria método Post create na classe CandidateController.

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,9 +2,18 @@ package br.com.vitorcarvalho.gestao_vagas.modules.candidate.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println("Candidato: ");
+        System.out.println(candidateEntity.getEmail());
+    }
     
 }
