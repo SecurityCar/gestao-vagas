65743f8a5c6e17c7344524ad352cf259a177ff29
Vitor Carvalho <vitorclocked@gmail.com>
2026-06-07 13:05:43 -0300

    feat(candidato): adiciona estrutura para transformar a classe CandidateEntity em um banco de dados.

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
@@ -1,17 +1,27 @@
 package br.com.vitorcarvalho.gestao_vagas.modules.candidate.controllers;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @Pattern(regexp = "\\S+", message = "O campo [username] não pode conter espaços.")
@@ -24,4 +34,7 @@ public class CandidateEntity {
 
     @Length(min = 10, max = 100)
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
