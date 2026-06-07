de7b1b63a967389e42fd7865dcfee22c405d4fee
Vitor Carvalho <vitorclocked@gmail.com>
2026-06-07 16:14:00 -0300

    feat: adiciona anotattion Service na classe CreateCandidateUseCase.

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -1,11 +1,13 @@
 package br.com.vitorcarvalho.gestao_vagas.modules.candidate.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.vitorcarvalho.gestao_vagas.exceptions.UserFoundException;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.controllers.CandidateEntity;
 
+@Service
 public class CreateCandidateUseCase {
     @Autowired
     private CandidateRepository candidateRepository;
