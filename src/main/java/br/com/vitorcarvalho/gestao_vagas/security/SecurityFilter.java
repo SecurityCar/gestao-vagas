ec4f9c2a6da972264fc93791b7dd7fb2141a99d6
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-07 20:27:25 -0300

    feat(authentication): cria sistema que utiliza o token como autentificador

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/security/SecurityFilter.java
@@ -1,10 +1,16 @@
 package br.com.vitorcarvalho.gestao_vagas.security;
 
 import java.io.IOException;
+import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.vitorcarvalho.gestao_vagas.providers.JWTProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,11 +19,28 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class SecurityFilter extends OncePerRequestFilter{
 
+    @Autowired
+    private JWTProvider jwtProvider;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        String header = request.getHeader("Authorization");
+        SecurityContextHolder.getContext().setAuthentication(null);
+
+        if(header != null){
+            var subject_token = this.jwtProvider.validadeToken(header);
+            if(subject_token.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subject_token);
+            UsernamePasswordAuthenticationToken auth = 
+            new UsernamePasswordAuthenticationToken(subject_token, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
+        filterChain.doFilter(request, response);
     }
     
 }
