eab02eae5fe61ce8d099f6f2af26a666cf35c254
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-07 15:08:17 -0300

    feat(authentication): adiciona sistema que gera token para caso a senha crua e a criptografada sejam compatíveis em AuthCompanyUseCase

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,9 +2,13 @@ package br.com.vitorcarvalho.gestao_vagas.modules.company.useCases;
 
 import javax.naming.AuthenticationException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.vitorcarvalho.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.vitorcarvalho.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -12,6 +16,9 @@ import br.com.vitorcarvalho.gestao_vagas.modules.company.repositories.CompanyRep
 @Service
 public class AuthCompanyUseCase {
     
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     private final PasswordEncoder passwordEncoder;
     private final CompanyRepository companyRepository;
 
@@ -20,7 +27,7 @@ public class AuthCompanyUseCase {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
             () -> {
                 throw new UsernameNotFoundException("Company not found.");
@@ -36,5 +43,11 @@ public class AuthCompanyUseCase {
         }
 
         //Se forem iguais -> Gerar token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("gestao_vagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+
+        return token;
     }
 }
