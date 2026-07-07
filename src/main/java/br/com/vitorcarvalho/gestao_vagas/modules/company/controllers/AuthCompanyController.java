b30e099f6f1f742a06bc648b9dfa28ed29c54e8c
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-07 15:28:09 -0300

    feat(controller): adiciona sistema de exceções para casos onde a senha/username estejam incorretas

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/controllers/AuthCompanyController.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/controllers/AuthCompanyController.java
@@ -2,6 +2,8 @@ package br.com.vitorcarvalho.gestao_vagas.modules.company.controllers;
 
 import javax.naming.AuthenticationException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,12 @@ public class AuthCompanyController {
     }
 
     @PostMapping("/company")
-    public String create(@RequestBody AuthCompanyDTO authCompanyDTO) throws AuthenticationException{
-        return this.authCompanyUseCase.execute(authCompanyDTO);
+    public ResponseEntity<Object> create(@RequestBody AuthCompanyDTO authCompanyDTO){
+        try{
+            var result = this.authCompanyUseCase.execute(authCompanyDTO);
+            return ResponseEntity.ok().body(result);
+        }catch(Exception ex){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+        }
     }
 }
