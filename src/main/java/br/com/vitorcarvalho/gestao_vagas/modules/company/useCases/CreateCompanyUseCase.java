f20b3eae16140bce7efa64250a30b5c16b70e02c
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-07 14:27:33 -0300

    feat(security): adiciona criptografia de senha de CompanyEntity na classe CreateCompanyUseCase

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -1,6 +1,7 @@
 package br.com.vitorcarvalho.gestao_vagas.modules.company.useCases;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.vitorcarvalho.gestao_vagas.exceptions.UserFoundException;
@@ -12,15 +13,21 @@ public class CreateCompanyUseCase {
     
     private final CompanyRepository companyRepository;
 
-    CreateCompanyUseCase(CompanyRepository companyRepository) {
+    CreateCompanyUseCase(CompanyRepository companyRepository, PasswordEncoder passwordEncoder) {
         this.companyRepository = companyRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    private final PasswordEncoder passwordEncoder;
 
     public CompanyEntity execute(CompanyEntity companyEntity){
         this.companyRepository.findByUsernameOrEmail(companyEntity.getUsername(), companyEntity.getEmail())
         .ifPresent((user) -> {
             throw new UserFoundException();
         });
+
+        var password = passwordEncoder.encode(companyEntity.getPassword());
+        companyEntity.setPassword(password);
 
         return this.companyRepository.save(companyEntity);
     }
