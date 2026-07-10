803f7301231a817b6e25a6fb8ead6fa30fd0df76
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-10 15:27:11 -0300

    feat: define retorno de erro

--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.vitorcarvalho.gestao_vagas.exceptions.UserFoundException;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.vitorcarvalho.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.vitorcarvalho.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
@@ -61,6 +62,14 @@ public class CandidateController {
     
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+     @ApiResponses({
+        @ApiResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(implementation = ProfileCandidateResponseDTO.class))
+        }),
+        @ApiResponse(responseCode = "400", description = "User not found")})
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
         var idCandidate = request.getAttribute("candidate_id");
         try {
--- src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/vitorcarvalho/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -2,6 +2,7 @@ package br.com.vitorcarvalho.gestao_vagas.modules.candidate.dto;
 
 import java.util.UUID;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProfileCandidateResponseDTO {
+    @Schema(example = "Roberto")
     private String name;
+
+    @Schema(example = "roberto")
     private String username;
+
+    @Schema(example = "roberto@gmail.com")
     private String email;
+
+    @Schema(example = "Desenvolvedor Kotlin")
     private String description;
     private UUID id;
 }
